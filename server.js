904400c1b08a4429a88b3f6a604ad3e89244a8b4
Your Name <you@example.com>
2022-10-19 17:51:49 -0500

    co

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 //crear la dependecia a expres
-const express=require('express')
+const express =require('express')
+const clors =require('colors')
+const dotenv =require('dotenv')
+const listEndpoints =require('express-list-endpoints')
+//dependencia a las rutas
+const boocampRoutes = require('./routes/BootcamspRoutes')
+const cursopRoutes = require('./routes/CursoRoutes')
 
+const app= express()
 
-const app= express
+//relacionar rutas de dominio
+app.use('/api/v1/bootcamps', boocampRoutes)
+//app.use('/api/v1/cursos', cursopRoutes)
+
+app.get('/', (request, response)=>{
+    response.send('ruta funcionando')
+})
+
+dotenv.config({
+    path: './config/config.env'
+})
+
+//listar rutas
+
+console.log(listEndpoints(app))
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`servidor iniciado en puerto: ${process.env.PORT}   `.bgGreen.white)
+})
 
