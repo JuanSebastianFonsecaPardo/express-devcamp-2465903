25ff1078b15b86f61103a19a1544ac4f9d533dce
Your Name <you@example.com>
2022-10-20 16:37:53 -0500

    hola

--- routes/BootcamspRoutes.js
+++ routes/BootcamspRoutes.js
@@ -1,30 +1,12 @@
 const express =require('express')
+const {getAllBootcamps, getBootcamps, getCreateBootcamps, 
+        getDeleteBootcamps, getSingleBootcamps, getUpdateBootcamps} = require('../controllers/BoocampController')
+
+
 const router =express.Router()
 
-//rutas para boocamps
-//listar bootcamps
-router.get('/', (req, res)=>{
-    res.status(200).json({"success" : true, "data" :"Aqui se van a mostrar todos los bootcamps"})
-})
+router.route('/').get(getAllBootcamps).post(getCreateBootcamps)
 
-
-//listar bootcamps por id
-router.get('/:id', (req, res)=>{
-    res.status(200).json({"success" : true, "data" :`Aqui se va a mostrar el bootcamp cuya id es ${req.params.id}`})
-})
-//crear bootcamps
-router.post('/', (req, res)=>{
-    res.status(201).json({"success" : true, "data" :"Aqui se va a crear un nuevo bootcamps"})
-})
-
-//actualizar bootcamps
-router.put('/:id', (req, res)=>{
-    res.status(200).json({"success" : true, "data" :`Aqui se va a arualizar el bootcamp cuya id es ${req.params.id}`})
-})
-
-//eliminar bootcamps
-router.delete('/:id', (req, res)=>{
-    res.status(200).json({"success" : true, "data" :`Aqui se va a eliminar el bootcamp cuya id es ${req.params.id}`})
-})
+router.route('/:id').get(getSingleBootcamps).put(getUpdateBootcamps).delete(getDeleteBootcamps)
 
 module.exports = router
