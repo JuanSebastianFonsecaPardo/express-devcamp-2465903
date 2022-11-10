1d8616dc46392f62e697b75238ef8d6fe22665f6
JuanCelis25 <104436922+JuanCelis25@users.noreply.github.com>
2022-11-10 16:16:58 -0500

    final

--- controllers/CoursesController.js
+++ controllers/CoursesController.js
@@ -1,42 +1,166 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes, ValidationError  } = require("sequelize");
 const CourseModel = require('../models/courses');
 const sequelize = require('../config/seq');
 
 const Courses = CourseModel(sequelize, DataTypes)
-//Listar todos los Courses
-    exports.getAllCourses =  async(req , res)=>{
+
+//Listar todos los cursos
+exports.getAllCourses =  async(req , res)=>{
+    try {
         const allCourses = await Courses.findAll()
-        res.status(200).json({"success" : true,"data": allCourses })
+        res
+        .status(200)
+        .json({
+            "success" : true,
+            "data": allCourses,
+        })
+    } catch (error) {
+        res.status(400).json({
+            "succes": false,
+            "errors": "Error base de datos"
+        })
     }
+    
+}
 
-//Listar un Course por id
-    exports.getSingleCourse = async (req, res)=>{
+
+//Listar un cursos por id
+exports.getSingleCourse = async (req, res)=>{
+    try {
+        //Seleccionar usuario por id
         const SingleCourse = await Courses.findByPk(req.params.id)
-        res.status(200).json({"success" : true,"data" :  SingleCourse})           
-    }
+        if (!SingleCourse) {
+            res.status(400).json({
+                "succes": false,
+                "errors": "Error usuario no existente"
+            })
+        }else{
+            //Enviar response
+        res
+        .status(200)
+        .json({
+            "success" : true,
+            "data" :  SingleCourse          
+        }) 
+        }
+    } catch (error) {
+        res.status(400).json({
+            "succes": false,
+            "errors": "Error base de datos"
+        })
+    }          
+}
 
-//Crear un Course
-    exports.createCourse = async(req, res)=>{
+//Crear un curso
+exports.createCourse = async(req, res)=>{
+    try {
+        //Grabar el nuevo curso
         const createCourse = await Courses.create(req.body);
-        res.status(201).json({"success" : true,"data": createCourse})
+        //Enviar la respuesta con el nuevo curso
+        res
+        .status(201)
+        .json({
+            "success" : true,
+            "data": createCourse
+        })
+    }catch (error) {
+        if (error instanceof ValidationError ) {
+            //Recorrer el arreglo de errores 
+            //map
+            const msgerrores = error.errors.map(errorItem=>errorItem.message)
+            res.status(422).json({
+                "succes": false,
+                "errors": msgerrores
+            })
+        }else{
+            res.status(400).json({
+                "succes": false,
+                "errors": "Error base de datos"
+            })
+        }
     }
-//Actualizar un Course por id
-    exports.updateCourse = async(req, res)=>{
+}
+
+//Actualizar un curso por id
+exports.updateCourse = async(req, res)=>{
+    try {
+        //Seleccionar el curso por id
+        const SingleCourse = await Courses.findByPk(req.params.id)
+        if (!SingleCourse) {
+            res.status(400).json({
+                "succes": false,
+                "errors": "Error usuario no existente"
+            })
+        }else{
+        //si existe actualizo
         await Courses.update(req.body ,{
             where: {
-                id : req.params.id
-              }
-        });
-        const SingleCourse = await Courses.findByPk(req.params.id)
-        res.status(200).json({"success" : true,"data": SingleCourse})
-    }
-//Borrar un Course por id
-    exports.deleteCourse = async(req, res)=>{
-        await Courses.destroy({
-            where: {
-              id: req.params.id
+              id : req.params.id
             }
           });
-          const SingleCourse = await Courses.findByPk(req.params.id)
-        res.status(200).json({"success" : true,"data": `El curso de id : ${req.params.id} fue eliminado`})
+        }
+        //Volvemos a seleccionar
+        const updateCourse = await Courses.findByPk(req.params.id)  
+        //response con el curso actualizado
+        res
+        .status(200)
+        .json({
+            "success" : true,
+            "data": updateCourse
+        })
+        }
+    catch (error) {
+        res.status(400).json({
+            "succes": false,
+            "errors": "Error base de datos"
+        })
     }
+}
+
+
+// //Borrar un curso por id
+// exports.deleteCourse = async(req, res)=>{
+//     try {
+//         //Seleccionar el curso por id
+//         const SingleCourse = await Courses.findByPk(req.params.id)
+//         //Si no existe 
+//         if (!SingleCourse) {
+//             res.status(400).json({
+//                 "succes": false,
+//                 "errors": "Error curso no existente"
+//             })
+//         }else{
+//         //si existe actualizo
+//         await Courses.destroy(req.body ,{
+//             where: {
+//               id : req.params.id
+//             }
+//           });
+//         }
+//         //Volvemos a seleccionar
+//         const deleteCourse = await Courses.findByPk(req.params.id)  
+//         //response con el usuario actualizado
+//         res
+//         .status(200)
+//         .json({
+//             "success" : true,
+//             "data": deleteCourse
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             "succes": false,
+//             "errors": "Error base de datos"
+//         })
+//     }
+// }
+
+//Borrar un Course por id
+exports.deleteCourse = async(req, res)=>{
+    await Courses.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+      const SingleCourse = await Courses.findByPk(req.params.id)
+    res.status(200).json({"success" : true,"data": `El curso de id : ${req.params.id} fue eliminado`})
+}
--- models/courses.js
+++ models/courses.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const {Model} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Courses extends Model {
     /**
@@ -14,13 +12,137 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Courses.init({
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    weeks: DataTypes.INTEGER,
-    enroll_cost: DataTypes.REAL,
-    minimun_skill: DataTypes.INTEGER
+    //Titulo
+    title:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate:{
+        isAlpha: {
+          args: true,
+          msg:"El titulo debe tener solo letras"
+        },
+        notNull: {
+          args: true,
+          msg:"El titulo no esta presente"
+        },
+        notEmpty: {
+          args: true,
+          msg:"El titulo no debe estar vacio"
+        }
+      }
+    },
+    //Descripcion
+    description:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate:{
+        isAlpha: {
+          args: true,
+          msg:"El Descripcion debe tener solo letras"
+        },
+        notNull: {
+          args: true,
+          msg:"El Descripcion no esta presente"
+        },
+        notEmpty: {
+          args: true,
+          msg:"El Descripcion no debe estar vacio"
+        }
+      }
+    },
+    //Semanas
+    weeks:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate:{
+        isNumeric: {
+          args: true,
+          msg:"las Semanas tener solo numeros"
+        },
+        max:{
+          args: 1,
+          msg:"las semanas tiene un maximo de 1 digitos"
+        },
+        min: {
+          args: 1,
+          msg:"las semanas debe tener al menos 1 digito"
+        },
+        notNull: {
+          args: true,
+          msg:"el numero de Semanas no esta presente"
+        },
+        notEmpty: {
+          args: true,
+          msg:"las Semanas no debe estar vacio"
+        }
+      }
+    },
+    //Costo
+    enroll_cost:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate:{
+        isFloat: {
+          args: true,
+          msg:"El Costo debe tener solo numeros"
+        },
+        notNull: {
+          args: true,
+          msg:"El Costo no esta presente"
+        },
+        notEmpty: {
+          args: true,
+          msg:"El Costo no debe estar vacio"
+        }
+      }
+    },
+    //Habilidades
+    minimun_skill:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate:{
+        isAlpha: {
+          args: true,
+          msg:"La habilidades debe tener solo letras"
+        },
+        notNull: {
+          args: true,
+          msg:"las habilidades no esta presente"
+        },
+        notEmpty: {
+          args: true,
+          msg:"las habilidades no debe estar vacio"
+        }
+      }
+    },
+    //Bootcamp
+    bootcamp_id:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate:{
+        isNumeric: {
+          args: true,
+          msg:"El Bootcamp_id debe tener solo numeros"
+        },
+        notNull: {
+          args: true,
+          msg:"El Bootcamp_id no esta presente"
+        },
+        notEmpty: {
+          args: true,
+          msg:"El Bootcamp_id no debe estar vacio"
+        }
+      }
+    }
   }, {
     sequelize,
+    timestamps: false,
     modelName: 'Courses',
   });
   return Courses;
